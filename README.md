0a61c3796a0a08aa6bf2144b83049677c8708a03
ianleto <ccssliu@163.com>
2021-09-08 19:34:54 +0800

    paht

--- README.md
+++ README.md
@@ -3,9 +3,8 @@ ian自用 py
 #### py 常用三方库
     - click py 命令行
 
-
-
 #### py 常用内部库
+    - python 项目路径
 
 
 #### py 如何打包
---
+++ utils/path.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/9/8 7:16 下午
+# @Author  : Ian Leto
+# @File    : path.py
+# 干啥的    : 路径相关方法
+
+import os
+
+# 当前路径
+os.getcwd()
+
+# 上级路径
+os.path.abspath('..')
+
