a58971febc35583f9946d9924a618af43355a6d5
ianleto <ccssliu@163.com>
2021-09-08 20:10:14 +0800

    增加路径相关方法

---
+++ structure/nodelist/base.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/9/8 8:07 下午
+# @Author  : Ian Leto
+# @File    : base.py
+# 干啥的    : 基础的链表
+
+
--- utils/path.py
+++ utils/path.py
@@ -6,9 +6,24 @@
 
 import os
 
+# 获取路径方式 eg utils/path/testPath
+rootPath = os.path.abspath('..')
+
+
+def get_root_path():
+    return os.path.abspath('..')
+
+
+def get_file_path(path: str):
+    return os.path.join(get_root_path(), path)
+
+
 # 当前路径
 os.getcwd()
 
 # 上级路径
 os.path.abspath('..')
 
+if __name__ == '__main__':
+    print(os.path.abspath('hello.py'))
+    print(os.getcwd())
