e0354a0c4006309d3ffb531dfe89dcb7c84e8bba
ianleto <ccssliu@163.com>
2022-09-16 18:06:14 +0800

    add 脉脉模板

--- scripts/prometheus.sh
+++ scripts/prometheus.sh
@@ -2,3 +2,5 @@
 docker pull prometheus
 docker run --name prometheus -p 9090:9090 -v /root/config/prom.yml:/etc/prometheus/prometheus.yml -d prom/prometheus
 docker run -d --name gra -p 3000:3000 grafana/grafana
+docker run -d --name pushgateway -p 9091:9091 prom/pushgateway
+wget https://github.com/prometheus/node_exporter/releases/download/v1.2.2/node_exporter-1.2.2.linux-amd64.tar.gz
